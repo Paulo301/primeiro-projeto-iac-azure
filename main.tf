c2e0e59e6d52e1c74c9e2351b352dc6344b1b51c
Paulo Victor Lemos de Almeida <pauloalmeida@ma.def.br>
2026-03-13 16:10:23 -0300

    feat: create vnet resource

--- main.tf
+++ main.tf
@@ -3,6 +3,19 @@ resource "azurerm_resource_group" "rg" {
   location = "brazilsouth"
 
   tags = {
-    Iac = true
+    Iac = "True"
   }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "rocketseat-vnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    Iac = "True"
+  }
+
+  depends_on = [azurerm_resource_group.rg]
 }
--- providers.tf
+++ providers.tf
@@ -9,6 +9,8 @@ terraform {
 
 provider "azurerm" {
   features {
-
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
   }
 }
